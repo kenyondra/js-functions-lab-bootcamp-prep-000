3cf2411c6baecebf4a9ef3e0474aea2caf2a36b6
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-05 03:10:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-// write your code below!
+// write your code below
 
-let name ='you';
-let holiday ='Independence Day';
-let number =20;
-let mother ='Mother's Day';
 function happyHolidays(){
   return "Happy holidays!"
 }  
@@ -17,6 +13,7 @@ function holidayCountdown(holiday, days){
   return 'It\'s ${days} days until ${holiday}!'
 }
 
-let name ='you'
-let holiday ='Independence Day'
+let name ='you';
+let holiday ='Independence Day';
 let number =20;
+let mother ='Mother's Day';
